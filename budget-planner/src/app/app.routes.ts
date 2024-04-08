43bd8c74d75646023f14177015d6cf7711ef6fc7
Jason <bayfie10@gmail.com>
2024-04-08 17:29:29 +0200

    Routing modules and login-form coponent generated.

--- budget-planner/src/app/app.routes.ts
+++ budget-planner/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'budget-planner', loadChildren: () => import('../app/budget-planner/budget-planner.module').then(map => map.BudgetPlannerModule)
+    }
+];
