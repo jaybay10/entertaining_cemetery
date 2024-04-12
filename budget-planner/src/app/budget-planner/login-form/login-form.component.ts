0f25707811b598a73ad276e9ccc7a58c7a63c5c3
Jason <bayfie10@gmail.com>
2024-04-12 15:27:02 +0200

    Login form validations and Signup form validations competed.

--- budget-planner/src/app/budget-planner/login-form/login-form.component.ts
+++ budget-planner/src/app/budget-planner/login-form/login-form.component.ts
@@ -1,24 +1,100 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-login-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './login-form.component.html',
-  styleUrl: './login-form.component.scss'
+  styleUrl: './login-form.component.scss',
 })
-export class LoginFormComponent {
+export class LoginFormComponent implements OnInit{
 
   @ViewChild('container', {static: true}) container!: ElementRef
   @ViewChild('signUp', { static: true }) signUpButton!: ElementRef;
   @ViewChild('signIn', {static: true}) signInButton!: ElementRef;
+  loginForm: FormGroup | any;
+  signUpForm: FormGroup | any;
 
-  SignUp(): void {   
+  constructor(private formBuilder: FormBuilder, private router: Router) {}
+
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      email: ["", [Validators.required, Validators.email]],
+      password: ["", [Validators.required, Validators.minLength(6)]]
+    });
+
+    this.signUpForm = this.formBuilder.group({
+      username: ["", [Validators.required, Validators.minLength(3)]],
+      email: ["", [Validators.required, Validators.email]],
+      password: ["", [Validators.required , Validators.minLength(6)]]
+    }); 
+  }
+
+  login(){
+    if(this.loginForm.valid){
+      console.log("Login info ---> ", this.loginForm.value);
+      this.router.navigate(["budget-planner/dashboard"]);
+    } else {
+      console.log("Pop up should be displayed here");
+    }
+  }
+
+  signup(){
+    if(this.signUpForm.valid){
+      console.log("Reegister info ---> ", this.signUpForm.value);
+      setTimeout(() => {
+        window.location.reload();
+      }, 2000);
+      this.router.navigate(["/budeget-planner/login"]);
+    } else {
+      console.log("Pop up should be displayed here");
+    }
+  }
+
+  isEmailLoginInvalid(): boolean {
+    return this.loginForm.get('email').touched && this.loginForm.get('email').hasError('email');
+}
+
+  isEmailSignUpInvalid(): boolean {
+    return this.signUpForm.get('email').touched && this.signUpForm.get('email').hasError('email');
+  }
+
+  isPasswordLoginInvalid(): boolean {
+    const password = this.loginForm.get('password').value;
+    if(password.length < 6){
+      return this.loginForm.get('password').touched && this.loginForm.get('password').hasError('minlength')
+    } else {
+      return false;
+    }
+  }
+
+  isUsernameSignUpInvalid(): boolean{
+    const username = this.signUpForm.get('username').value;
+    if(username.length < 3){
+      return this.signUpForm.get('username').touched && this.signUpForm.get('username').hasError('minlength');
+    } else {
+      return false;
+    }
+  }
+
+  isPasswordSignUpInvalid(): boolean {
+    const password = this.signUpForm.get('password').value;
+    if(password.length < 6){
+      return this.signUpForm.get('password').touched && this.signUpForm.get('password').hasError('minlength')
+    } else {
+      return false;
+    }
+  }
+
+  signUpDetails(): void {   
     this.container.nativeElement.classList.add('active');
+ 
   }
 
-  SignIn(): void {
+  loginDetails(): void {
     this.container.nativeElement.classList.remove('active')
+    //this.signUpForm.reset();
   }
-
 }
